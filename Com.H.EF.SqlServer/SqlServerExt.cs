836391a7a4d59710496729f7c21e783d78fa99d0
Hussein Al Bayati <hussein@albayati.com>
2022-01-18 08:55:18 +0400

    Add project files.

---
+++ Com.H.EF.SqlServer/SqlServerExt.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.H.EF.SqlServer
+{
+    public static class SqlServerExt
+    {
+        public static DbContext NewDbContext(this string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new DbContext(optionsBuilder.Options);
+        }
+    }
+}
